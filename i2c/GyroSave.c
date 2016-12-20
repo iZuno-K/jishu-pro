c9f8117ca15b55e1fab4e8ecf932d7130820dffc
iZuno-K <ielephantseasidei@gmail.com>
2016-12-20 16:54:03 +0900

    file outputs

--- i2c/GyroSave.c
+++ i2c/GyroSave.c
@@ -10,22 +10,20 @@
 #include <stdint.h>
 #include <wiringPi.h> //delay関数用
 
-// #include <sys/time.h>
-
 void L3GD20_readData(int *gyroData, int fd);
 void L3GD20_write(unsigned char address, unsigned char data, int fd);
 unsigned char L3GD20_read(unsigned char address, int fd);
 void L3GD20_init(int fd);
-
 
 int main(int argc, char **argv){
   int i2c_fd; //i2cへのファイルディスクプタ
   char* i2cFileName = "/dev/i2c-1";//リビジョンに合わせて変更
   int i2cAddress = 0x6a;
   int gyroData[3]; //(x,y,z)
+  float x,y,z; //degree per seconds
+  FILE *fp;
+  char *fname = "gyroData.csv"
 
-  // struct timeval startTime, endTime;  // 構造体宣言
-  // double realsec;
 
   printf("i2c Gyro(L3GD20) test program\n");
   delay(500);
@@ -41,21 +39,27 @@ int main(int argc, char **argv){
   }
   //デバイス初期化
   L3GD20_init(i2c_fd);
-  //１秒毎,２０回センサ情報取得
-  int i;
-  // gettimeofday(&startTime, NULL);     // 開始時刻取得
 
+  fp = fopen( fname, "w" );
+  if( fp == NULL ){
+    printf( "%sファイルが開けません¥n", fname );
+    return -1;
+  }
+  fprintf(fp, "%c,%c,%c",'x','y','z');
+  //１ms毎,２０回センサ情報取得
+  int i;
   for (i=0;i<1000;i++){
     L3GD20_readData(gyroData, i2c_fd);
-    // gettimeofday(&endTime, NULL);       // 終了時刻取得
-    // time_t diffsec = difftime(endTime.tv_sec, startTime.tv_sec);    // 秒数の差分を計算
-    // suseconds_t diffsub = endTime.tv_usec - startTime.tv_usec;      // マイクロ秒部分の差分を計算
-    // realsec = diffsec+diffsub*1e-6; // 実時間を計算
     //データを校正して表示
-    printf("(x, y, z) time = (%5.2f, %5.2f, %5.2f) \n",
-    (float)gyroData[0]*0.00875, (float)gyroData[1]*0.00875, (float)gyroData[2]*0.00875);
+    x = (float)gyroData[0]*0.00875;
+    y = (float)gyroData[1]*0.00875;
+    z = (float)gyroData[2]*0.00875);
+    printf("(x, y, z) time = (%5.2f, %5.2f, %5.2f) \n", x,y,z);
+    fprintf(fp, "%5.2f, %5.2f, %5.2f\n", x,y,z);
     delay(1);
   }
+
+  fclose(fp);
   return;
 }
 
